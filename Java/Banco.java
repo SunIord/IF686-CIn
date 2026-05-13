5c62e0fdf0e365a1cc85b1ea64823d3dbe98d7bf
João Pedro A. de Moraes <160938086+SunIord@users.noreply.github.com>
2026-05-13 12:31:47 -0300

    Refactor Banco class to use RepositorioContas

--- Java/Banco.java
+++ Java/Banco.java
@@ -1,78 +1,73 @@
 public class Banco {
-    private ContaAbstrata[] contas;
-    private int indice;
-    private double  taxaJuros;
+    private RepositorioContas repositorio;
+    private double taxaJuros;
 
-    public Banco () {
-        contas = new ContaAbstrata[100];
-        indice = 0;
-        taxaJuros = 0.01;
+    public Banco(RepositorioContas repositorio) {
+        this.repositorio = repositorio;
+        this.taxaJuros = 0.01;
     }
 
     public void cadastrar(ContaAbstrata conta) {
-        contas[indice] = conta;
-        indice++;
+        repositorio.inserir(conta);
     }
 
     public void creditar(String numero, double valor) {
-        for (int i = 0; i < indice; i++) {
-            if (contas[i].getNumero().equals(numero)) {
-                contas[i].creditar(valor);
-            }    
+        ContaAbstrata conta = repositorio.procurar(numero);
+
+        if (conta != null) {
+            conta.creditar(valor);
+            repositorio.atualizar(conta);
         }
-    } 
+    }
 
     public void debitar(String numero, double valor) {
-        for (int i = 0; i < indice; i++) {
-            if (contas[i].getNumero().equals(numero)) {
-                contas[i].debitar(valor);
-            }
+        ContaAbstrata conta = repositorio.procurar(numero);
+
+        if (conta != null) {
+            conta.debitar(valor);
+            repositorio.atualizar(conta);
         }
     }
 
     public double getSaldo(String numero) {
-        for (int i = 0; i < indice; i++) {
-            if (contas[i].getNumero().equals(numero)) {
-                return contas[i].getSaldo();
-            }
+        ContaAbstrata conta = repositorio.procurar(numero);
+
+        if (conta != null) {
+            return conta.getSaldo();
         }
+
         return 0;
     }
 
     public void renderJuros(String numero) {
-        for (int i =0; i < indice; i++) {
-            if (contas[i].getNumero().equals(numero)) {
-                double saldo = contas[i].getSaldo();
-                double juros = saldo * taxaJuros;
-                contas[i].creditar(juros);
-            }
+        ContaAbstrata conta = repositorio.procurar(numero);
+
+        if (conta != null) {
+            double juros = conta.getSaldo() * taxaJuros;
+            conta.creditar(juros);
+            repositorio.atualizar(conta);
         }
     }
 
     public void renderBonus(String numero) {
-        for (int i = 0; i < indice; i++) {
-            if (contas[i].getNumero().equals(numero)) {
-                if (contas[i] instanceof ContaEspecial ce) {
-                    ce.renderBonus(); 
-                }
-            }
+        ContaAbstrata conta = repositorio.procurar(numero);
+
+        if (conta instanceof ContaEspecial ce) {
+            ce.renderBonus();
+            repositorio.atualizar(conta);
         }
     }
 
     public void transferir(String origem, String destino, double valor) {
-        ContaAbstrata contaOrigem = null;
-        ContaAbstrata contaDestino = null;
-        for (int i = 0; i < indice; i++) {
-            if (contas[i].getNumero().equals(origem)) {
-                contaOrigem = contas[i];
-            }
-            if (contas[i].getNumero().equals(destino)) {
-                contaDestino = contas[i];
-            }
-        }
+        ContaAbstrata contaOrigem = repositorio.procurar(origem);
+        ContaAbstrata contaDestino = repositorio.procurar(destino);
+
         if (contaOrigem != null && contaDestino != null) {
             contaOrigem.debitar(valor);
             contaDestino.creditar(valor);
+
+            repositorio.atualizar(contaOrigem);
+            repositorio.atualizar(contaDestino);
         }
     }
 }
